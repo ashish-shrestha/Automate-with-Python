cfcbe0d557c166886d6d999af60616145fd62712
Ashish Shrestha <ashishamrit@gmail.com>
2016-12-24 12:27:44 -0500

    fixed code error and added comments to understand each line

--- Python/fileSorting.py
+++ Python/fileSorting.py
@@ -1,12 +1,12 @@
 import os #imports methods and functions necessary to manipulate the system
-currentDirectory = os.getcwd()
-listOfItems = os.listdir(path=currentDirectory)
-for item in listOfItems:
-	nameSeparated = item.split()
-	firstWord = nameSeparated[0]
-	firstLetter = firstWord[0]
-	if os.path.isdir('./' + str(firstLetter)) == True:
-		os.rename((str(currentDirectory) + '/' + item), ((str(currentDirectory) + '/' + str(firstLetter) + '/' + item))
-	else:
-		os.mkdir('./' + str(firstLetter))
-		os.rename((str(currentDirectory) + '/' + item), ((str(currentDirectory) + '/' + str(firstLetter) + '/' + item))
+currentDirectory = os.getcwd() #creates a variable currentDirectory in which the currentDirectory path is stored
+listOfItems = os.listdir(path=currentDirectory) #creates a list of all items in current directory
+for item in listOfItems: #looping over each item in current directory
+	firstLetter = item[0] #grabs first letter to determine where item will be placed
+	firstLetter = firstLetter.upper() #shifts letter to uppercase to avoid separation of items like 'python' and 'Python' (case-sensitive)
+	if os.path.isdir(str(currentDirectory) + '/' + str(firstLetter)) == True: #if directory already exists...
+		os.rename((str(currentDirectory) + '/' + item), (str(currentDirectory) + '/' + str(firstLetter) + '/' + item)) #simply move the file/folder
+	else: #if directory doesn't exist
+		os.mkdir(str(currentDirectory) + '/' + str(firstLetter)) #make the directory
+		os.rename((str(currentDirectory) + '/' + item), (str(currentDirectory) + '/' + str(firstLetter) + '/' + item)) #and then move the file/folder
+
